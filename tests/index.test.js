459cca889e88f442a5c5b69a2281f129d0e4da33
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-08 17:14:19 +0100

    test toBeInstanceOf an Array

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,19 @@
 import request from 'supertest'
 import app from '../src/app.js' // <-- server
 
-describe("Check if '/tasks' GET route exists", () => {
+describe("Check '/tasks' GET route", () => {
 
   test('Should respond with a 200 status code', async () => {
     const response = await request(app).get('/tasks').send()
     // console.log(response)
 
     expect(response.statusCode).toBe(200)
+  })
+
+  test('Should respond with an array', async () => {
+    const response = await request(app).get('/tasks').send()
+
+    expect(response.body).toBeInstanceOf(Array)
   })
 
 })
