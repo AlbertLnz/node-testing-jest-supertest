7dc4f21d0c83e4d9f0a4becc013eac51b47fd4be
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-08 16:20:31 +0100

    GET example

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import express from 'express'
 const app = express()
 const PORT = 3000
 
+app.get('/ping', (_req, res) => {
+  res.send('pong')
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
