3f30ce7cd7dc1cf980596aa12e8a469b6a4c8e1f
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-08 16:50:14 +0100

    first test failed it & package.json test configured w/ ECMA Script Modules

---
+++ tests/index.test.js
@@ -0,0 +1,13 @@
+import request from 'supertest'
+import app from '../src/app.js' // <-- server
+
+describe("Check if '/tasks' GET route exists", () => {
+
+  test('Should respond with a 200 status code', async () => {
+    const response = await request(app).get('/tasks').send()
+    // console.log(response)
+
+    expect(response.statusCode).toBe(200)
+  })
+
+})
